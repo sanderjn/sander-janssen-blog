ed6133cbf12cf1c4d581d8149f05b963fba602bc
sanderjn <sander@sanderjanssen.nl>
2020-06-24 17:29:00 +0200

    Added cool blog page animation

--- components/Skew.vue
+++ components/Skew.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="absolute -my-10 md:-my-16 w-full h-full overflow-hidden z-0">
-    <SkewTop class="absolute top-0 w-full h-auto" style="transform:scale(1.03)"/>
+    <SkewTop class="skewclass skewtop absolute top-0 w-full h-auto"/>
     <!-- <SkewRight class="absolute hidden lg:block top-0 right-0 h-full w-auto" style="transform:scale(1.03)"/> -->
-    <SkewBottom class="absolute bottom-0 w-full h-auto" style="transform:scale(1.03)"/>
+    <SkewBottom class="skewclass skewbottom absolute bottom-0 w-full h-auto"/>
     <!-- <SkewLeft class="absolute hidden lg:block top-0 left-0 h-full w-auto" style="transform:scale(1.03)"/> -->
   </div>
 </template>
