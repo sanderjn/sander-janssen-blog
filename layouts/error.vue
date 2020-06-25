7da1d20ca355ee5f2f4c4855d058966ed34f545f
sanderjn <sander@sanderjanssen.nl>
2020-06-25 15:58:55 +0200

    Added 404

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Pagina niet gevonden</h1>
+    <h1 v-else>Whoops, er ging iets mis</h1>
+    <nuxt-link to="/">Terug naar de homepage</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
