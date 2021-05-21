b1205a6d473a96f57f15729bc686e942df4146b8
sanderjn <sander@sanderjanssen.nl>
2021-05-21 14:47:00 +0200

    Rollback to latest working version

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -70,6 +70,7 @@ export default {
           image: 'drumlin-distillery.jpg',
           url: 'drumlindistillery.com',
         },
+<<<<<<< HEAD
         {
           title: 'Else Schaaij',
           categories: 'Web design & development',
@@ -78,6 +79,9 @@ export default {
           url: 'elseschaaij.nl',
         },
         {
+=======
+        { 
+>>>>>>> parent of 630a708 (Added Else to portfolio)
           title: 'Relajate Lobitos',
           categories: 'Web design & development',
           description: 'WordPress website I did for a hostel, restaurant & surfshop in Peru while traveling South America.',
