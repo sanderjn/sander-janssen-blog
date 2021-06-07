a00244cbf2070a04ebcb15abc9a425f388630eff
sanderjn <sander@sanderjanssen.nl>
2021-06-07 12:49:11 +0200

    Updated sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,16 +77,7 @@ export default {
   },
   sitemap: {
     hostname: 'https://www.sanderjanssen.nl',
-    path: '/sitemap.xml',
-    routes() {
-      '/blog';
-      const fs = require('fs');
-      return fs.readdirSync('./assets/content/writing').map((file) => {
-        return {
-          route: `/blog/${path.parse(file).name}`
-        };
-      });
-    }
+    path: '/sitemap.xml'
   },
   generate: {
     routes() {
