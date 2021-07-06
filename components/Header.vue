f15b5685b91ce12617f0c9be1a7bc371bbf5857a
sanderjn <sander@sanderjanssen.nl>
2021-07-06 18:44:49 +0200

    Updated hover transition

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
   <header class="container flex items-center justify-between py-3 md:py-4">
     <nuxt-link class="flex items-end space-x-4 text-lg font-black leading-none sm:text-xl" to="/">
     <div class="w-16 h-16 rounded-full shadow-md bg-gradient-to-b from-white to-gray-50 overflow-hidden">
-      <img src="~/assets/images/sander.png" alt="Sander Janssen" class="w-full h-full object-cover transform transition-transform hover:rotate-12">
+      <img src="~/assets/images/sander.png" alt="Sander Janssen" class="w-full h-full object-cover transform transition-transform hover:scale-110 hover:-rotate-6">
     </div>
       <!-- <span class="text-white font-display">Sander Janssen</span> -->
     </nuxt-link>
