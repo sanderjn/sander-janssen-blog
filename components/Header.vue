4fb66eda0fb798ae2f9785a58d6511a9500a2db3
sanderjn <sander@sanderjanssen.nl>
2021-07-06 18:17:26 +0200

    Updated profile pic

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="container flex items-center justify-between py-3 md:py-4">
     <nuxt-link class="flex items-end space-x-4 text-lg font-black leading-none sm:text-xl" to="/">
-    <div class="w-16 h-16 rounded-full shadow-md bg-primary-500 overflow-hidden">
-      <img src="~/assets/images/sander-photo-cutout.png" alt="Sander Janssen" class="w-full h-full object-cover transform -rotate-6 transition-transform hover:rotate-6">
+    <div class="w-16 h-16 rounded-full shadow-md bg-gradient-to-b from-white to-gray-50 overflow-hidden">
+      <img src="~/assets/images/sander.png" alt="Sander Janssen" class="w-full h-full object-cover transform transition-transform hover:rotate-12">
     </div>
       <!-- <span class="text-white font-display">Sander Janssen</span> -->
     </nuxt-link>
@@ -11,12 +11,10 @@
 </template>
 
 <script>
-import Logo from '~/assets/images/svg/logo.svg';
 import HeaderNav from '~/components/HeaderNav';
 
 export default {
   components: {
-    Logo,
     HeaderNav,
   },
   methods: {
