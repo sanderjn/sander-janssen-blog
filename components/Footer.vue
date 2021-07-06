b374ecb041293d4653eac9473650ee4bb28bc54f
sanderjn <sander@sanderjanssen.nl>
2021-07-06 19:30:27 +0200

    Added name to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="container my-4">
-    <p class="mb-0 text-sm text-white">Built with <a class="underline" href="https://nuxtjs.org">Nuxt.js</a> and <a class="underline" href="https://tailwindcss.com/">TailwindCSS</a></p>
+    <p class="mb-0 text-sm text-white">By Sander Janssen - Built with <a class="underline" href="https://nuxtjs.org">Nuxt.js</a> and <a class="underline" href="https://tailwindcss.com/">TailwindCSS</a></p>
   </footer>
 </template>
 
