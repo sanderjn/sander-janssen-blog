3bffc0e4055af980e657c04e4a4169758a255cc2
sanderjn <sander@sanderjanssen.nl>
2021-07-06 20:00:58 +0200

    Added portfolio link

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
       </div>
       <div class="mt-10">
         <div class="prose">
-          <p>I'm doing freelance work and building smaller personal projects on the side, some of which can be found on <svg class="inline w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <p>I'm doing <nuxtLink to="/portfolio">freelance work</nuxtLink> and building smaller personal projects on the side, some of which can be found on <svg class="inline w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
             </svg>
             <a href="https://github.com/sanderjn" target="_blank" class="hover:text-primary-500">GitHub</a>. I'm currently open for new projects, so if you're interested in working with me, <a href="mailto:hello@sanderjanssen.nl" class="hover:text-primary-500">shoot me a message</a>.</p>
