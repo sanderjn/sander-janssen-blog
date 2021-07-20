600b22d85f3f8932fd2fa2e0d5aec294f87f0195
sanderjn <sander@sanderjanssen.nl>
2021-07-20 17:24:28 +0200

    Update WorkBanner.vue

--- components/WorkBanner.vue
+++ components/WorkBanner.vue
@@ -2,7 +2,7 @@
     <div class="relative">
         <img src="~/assets/images/sander-body.png" alt="Sander Janssen" class="hidden absolute w-32 bottom-0 right-8 z-0 sm:block">
         <div class="px-4 py-8 mt-10 rounded bg-gray-50 space-y-4 relative z-10 overflow-hidden md:px-8 md:space-y-0 sm:text-center md:flex md:justify-between md:items-center">
-            <h3 class="text-xl text-gray-800 font-display md:text-2xl">🤝 Work with me?</h3>
+            <h3 class="text-lg text-gray-800 font-display sm:text-2xl">🤝 Work with me?</h3>
             <a class="inline-block bg-primary-500 px-8 py-2 rounded-full font-medium text-white antialiased transition-colors hover:bg-primary-600" href="mailto:hello@sanderjanssen.nl">Say hello</a>
             <img src="~/assets/images/sander-body.png" alt="Sander Janssen" class="block absolute w-20 top-0 right-4 z-0 sm:hidden">
         </div>
