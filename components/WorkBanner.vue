3067e69d359e24f32111c8854b61e9daac73681b
sanderjn <sander@sanderjanssen.nl>
2021-07-21 16:14:03 +0200

    Button inline-block

--- components/WorkBanner.vue
+++ components/WorkBanner.vue
@@ -3,7 +3,7 @@
         <img src="~/assets/images/sander-body.png" alt="Sander Janssen" class="hidden absolute w-32 bottom-0 right-8 z-0 transition-transform sm:block group-hover:-translate-y-2">
         <div class="px-4 py-8 mt-10 rounded bg-gray-50 space-y-4 relative z-10 overflow-hidden md:px-8 md:space-y-0 sm:text-center md:flex md:justify-between md:items-center">
             <h3 class="text-lg text-gray-800 font-display sm:text-2xl">🤝 Work with me?</h3>
-            <a class="block bg-primary-500 px-6 py-3 rounded-full font-mono text-sm font-medium text-white antialiased transition-colors hover:bg-primary-600" href="mailto:hello@sanderjanssen.nl">Say hello</a>
+            <a class="inline-block bg-primary-500 px-6 py-3 rounded-full font-mono text-sm font-medium text-white antialiased transition-colors hover:bg-primary-600" href="mailto:hello@sanderjanssen.nl">Say hello</a>
             <img src="~/assets/images/sander-body.png" alt="Sander Janssen" class="block absolute w-20 top-0 right-4 z-0 sm:hidden">
         </div>
     </div>
