f7f1223d774b6a769b6dbd6deb5b0d65b804964e
sanderjn <sander@sanderjanssen.nl>
2023-06-05 12:52:39 +0200

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -1,122 +1,114 @@
 <template>
-  <div>
-    <!-- <Skew /> -->
-    <main class="z-10 content">
-      <div class="prose prose-sm md:prose">
-        <h1 class="inline-block px-4 -ml-4 !my-0">
-          👋🏻 Hi, I'm <br class="md:hidden" />
-          <span
-            class="inline-block px-1 transform bg-white rounded-sm -rotate-1"
-            ><span class="inline-block transform rotate-1"
-              >Sander Janssen</span
-            ></span
-          >
-        </h1>
-        <p class="block font-mono text-lg md:text-xl text-primary-600 !mt-3">
-          Front-end web developer <br class="md:hidden" /><span
-            class="text-gray-400"
-            >marketing background</span
-          >
-        </p>
-      </div>
-      <div class="mt-10">
-        <div class="prose prose-sm md:prose max-w-none">
-          <p>
-            I'm doing <nuxtLink to="/portfolio">freelance work</nuxtLink> and
-            building smaller personal projects on the side, some of which can be
-            found on
-            <svg
-              class="inline w-4 h-4"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-              />
-            </svg>
-            <a
-              href="https://github.com/sanderjn"
-              target="_blank"
-              class="hover:text-primary-600"
-              >GitHub</a
-            >. I'm currently open for new projects, so if you're interested in
-            working with me,
-            <a
-              href="mailto:hello@sanderjanssen.nl"
-              class="hover:text-primary-600"
-              >shoot me a message</a
-            >.
-          </p>
-          <h2>Work with me on</h2>
-          <ul>
-            <li v-for="skill in skills">{{ skill }}</li>
-          </ul>
-        </div>
-        <WorkBanner />
-        <div class="mt-10 prose prose-sm md:prose max-w-none">
-          <h2>More about me</h2>
-          <p>
-            I love music (disco/house/electronic) and I spend most of my free
-            time looking for new music to use in my DJ sets, which you can check
-            out on
-            <svg
-              class="inline w-4 h-4"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-              />
-            </svg>
-            <a
-              href="https://soundcloud.com/sanderjanssen"
-              target="_blank"
-              class="hover:text-primary-500"
-              >SoundCloud</a
-            >.
-          </p>
-        </div>
-      </div>
-    </main>
-  </div>
+    <div>
+        <!-- <Skew /> -->
+        <main class="z-10 content">
+            <div class="prose prose-sm md:prose">
+                <h1 class="inline-block px-4 -ml-4 !my-0">
+                    <span>👋🏻 Hi, I'm </span>
+                    <br class="md:hidden" />
+                    <span>Sander Janssen</span>
+                </h1>
+                <p
+                    class="block font-mono text-lg md:text-xl text-primary-600 !mt-3"
+                >
+                    Front-end web developer
+                </p>
+            </div>
+            <div class="mt-10">
+                <div class="prose prose-sm md:prose max-w-none">
+                    <p>
+                        I've got 10+ years of experience in building fast and
+                        user friendly
+                        <nuxtLink to="/portfolio"
+                            >websites that convert</nuxtLink
+                        >.
+                    </p>
+                    <h2>Work with me on</h2>
+                    <ul>
+                        <li v-for="skill in skills">{{ skill }}</li>
+                    </ul>
+                </div>
+                <WorkBanner />
+                <div class="mt-10 prose prose-sm md:prose max-w-none">
+                    <h2>More about me</h2>
+                    <p>
+                        When I'm not building websites, I'm probably spending my
+                        time:
+                    </p>
+                    <ul>
+                        <li v-for="hobby in hobbies">{{ hobby }}</li>
+                    </ul>
+
+                    <p>
+                        Also, I love music (disco/house/electronic) and I spend
+                        a large amount of my free time looking for new music to
+                        use in my DJ sets, which you can check out on
+                        <svg
+                            class="inline w-4 h-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                            />
+                        </svg>
+                        <a
+                            href="https://soundcloud.com/sanderjanssen"
+                            target="_blank"
+                            class="hover:text-primary-500"
+                            >SoundCloud</a
+                        >.
+                    </p>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script>
 import WorkBanner from "~/components/WorkBanner.vue";
 
 export default {
-  data() {
-    return {
-      skills: [
-        "Front-end web development",
-        "WordPress development",
-        "Conversion Rate Optimization",
-      ],
-    };
-  },
-  head() {
-    return {
-      title: this.title,
-      bodyAttrs: {
-        class: "home",
-      },
-    };
-  },
-  components: {
-    WorkBanner,
-  },
+    data() {
+        return {
+            logos: [
+                "kleinstesoepfabriek",
+                "boonstra",
+                "worldnauticcenter",
+                "worck",
+                "smaakgroenten",
+                "zweedsepellets.png",
+            ],
+            skills: [
+                "Front-end web development",
+                "WordPress development",
+                "Conversion Rate Optimization",
+                "UX design",
+            ],
+            hobbies: [
+                "🏄🏻‍♂️ Surfing",
+                "🤖 Playing with AI",
+                "🎧 Listening to a podcast",
+            ],
+        };
+    },
+    head() {
+        return {
+            title: this.title,
+            bodyAttrs: {
+                class: "home",
+            },
+        };
+    },
+    components: {
+        WorkBanner,
+    },
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,99 +1,69 @@
 <template>
-  <div>
-    <!-- <Skew /> -->
-    <!-- <div class="fixed bottom-0 z-0 w-full pb-2 text-sm text-center text-white opacity-75">↓ Scroll voor meer</div> -->
-    <main>
-      <div class="prose-sm md:prose !max-w-none">
-        <h1 class="!my-0">Portfolio</h1>
-        <p>Some recent work</p>
-        <div class="mt-10 space-y-16">
-          <div
-            v-for="portfolioItem in portfolioItems"
-            class="
-              bg-white
-              grid
-              md:grid-cols-2
-              items-center
-              rounded-md
-              overflow-hidden
-            "
-          >
-            <div class="p-8 prose">
-              <h2 class="!my-0">{{ portfolioItem.title }}</h2>
-              <p class="my-0 text-sm font-mono font-medium text-primary-600">
-                {{ portfolioItem.categories }}
-              </p>
-              <p>{{ portfolioItem.description }}</p>
-              <p class="mb-0">
-                <svg
-                  class="inline w-4 h-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                  />
-                </svg>
-                <a
-                  :href="'https://' + portfolioItem.url"
-                  target="_blank"
-                  class="text-sm"
-                  >{{ portfolioItem.url }}</a
-                >
-              </p>
+    <div>
+        <!-- <Skew /> -->
+        <!-- <div class="fixed bottom-0 z-0 w-full pb-2 text-sm text-center text-white opacity-75">↓ Scroll voor meer</div> -->
+        <main>
+            <div class="prose-sm md:prose !max-w-none">
+                <h1 class="!my-0">Portfolio</h1>
+                <p>Some recent work</p>
             </div>
-            <a
-              :href="'https://' + portfolioItem.url"
-              target="_blank"
-              class="
-                block
-                w-full
-                group
-                aspect-w-1 aspect-h-1
-                relative
-                overflow-hidden
-              "
-            >
-              <img
-                class="
-                  !my-0
-                  w-full
-                  h-full
-                  object-cover
-                  transform
-                  transition-transform
-                  group-hover:scale-110
-                "
-                :src="getPortfolioImgUrl(portfolioItem.image)"
-                alt=""
-              />
-              <div
-                class="
-                  absolute
-                  inset-0
-                  bg-gradient-to-b
-                  from-transparent
-                  to-black
-                  pointer-events-none
-                  opacity-60
-                  transition-opacity
-                  group-hover:opacity-0
-                "
-              ></div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="sm:mt-40 md:mt-56">
-        <WorkBanner />
-      </div>
-    </main>
-  </div>
+            <div class="mt-10 space-y-16">
+                <div
+                    v-for="portfolioItem in portfolioItems"
+                    class="bg-white grid md:grid-cols-2 items-center rounded-md overflow-hidden"
+                >
+                    <div class="p-8 prose">
+                        <h2 class="!my-0">{{ portfolioItem.title }}</h2>
+                        <p
+                            class="my-0 text-sm font-mono font-medium text-primary-600"
+                        >
+                            {{ portfolioItem.categories }}
+                        </p>
+                        <p>{{ portfolioItem.description }}</p>
+                        <p class="mb-0">
+                            <svg
+                                class="inline w-4 h-4"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                                />
+                            </svg>
+                            <a
+                                :href="'https://' + portfolioItem.url"
+                                target="_blank"
+                                class="text-sm"
+                                >{{ portfolioItem.url }}</a
+                            >
+                        </p>
+                    </div>
+                    <a
+                        :href="'https://' + portfolioItem.url"
+                        target="_blank"
+                        class="block w-full group aspect-w-1 aspect-h-1 relative overflow-hidden"
+                    >
+                        <img
+                            class="!my-0 w-full h-full object-cover transform transition-transform group-hover:scale-110"
+                            :src="getPortfolioImgUrl(portfolioItem.image)"
+                            alt=""
+                        />
+                        <div
+                            class="absolute inset-0 bg-gradient-to-b from-transparent to-black pointer-events-none opacity-60 transition-opacity group-hover:opacity-0"
+                        ></div>
+                    </a>
+                </div>
+            </div>
+            <div class="sm:mt-40 md:mt-56">
+                <WorkBanner />
+            </div>
+        </main>
+    </div>
 </template>
 <script>
 import Header from "~/components/Header.vue";
@@ -103,102 +73,103 @@ import WorkBanner from "~/components/WorkBanner.vue";
 import Vue from "vue";
 
 export default {
-  components: {
-    Header,
-    Footer,
-    WorkBanner,
-  },
-  data() {
-    return {
-      prefix: "portfolio",
-      title: "Portfolio - Sander Janssen",
-      portfolioItems: [
-        {
-          title: "Boonstra Marketing",
-          categories: "Web development",
-          description:
-            "Custom WordPress theme for Boonstra Marketing. Design by designpraktijk.nl",
-          image: "boonstramarketing.jpg",
-          url: "boonstramarketing.nl",
-        },
-        {
-          title: "Advocatenkantoor Vos",
-          categories: "Web development",
-          description:
-            "Custom WordPress theme for Advocatenkantoor Vos. Design by worck.nl",
-          image: "advocatenkantoor-vos.jpg",
-          url: "advocatenkantoorvos.nl",
-        },
-        {
-          title: "Smaak Groenten",
-          categories: "Web design & development",
-          description:
-            "Custom WordPress theme with WooCommerce integration for Smaak Groenten.",
-          image: "smaakgroenten.jpg",
-          url: "smaakgroenten.nl",
-        },
-        {
-          title: "Chloé - Events & Productie",
-          categories: "Web design & development",
-          description:
-            "Custom carrd.co website for an independent event & production agency.",
-          image: "chloe-events.jpg",
-          url: "chloe-events.nl",
-        },
-        {
-          title: "Zweedse Pellets",
-          categories: "Web design & development",
-          description:
-            "WordPress E-commerce website using a custom WordPress theme and custom Gutenberg blocks.",
-          image: "zweedsepellets.jpg",
-          url: "zweedsepellets.nl",
-        },
-        {
-          title: "Drumlin Distillery",
-          categories: "Web design & development",
-          description:
-            "WordPress E-commerce website using a custom WordPress theme and custom Gutenberg blocks.",
-          image: "drumlin-distillery.jpg",
-          url: "drumlindistillery.com",
-        },
-        {
-          title: "Else Schaaij",
-          categories: "Web design & development",
-          description: "WordPress website for a flute teacher.",
-          image: "elseschaaij.jpg",
-          url: "elseschaaij.nl",
-        },
-        {
-          title: "Relajate Lobitos",
-          categories: "Web design & development",
-          description:
-            "WordPress website for a hostel, restaurant & surfshop in Peru.",
-          image: "relajate-home.jpg",
-          url: "relajatelobitos.com",
-        },
-      ],
-    };
-  },
-  head() {
-    return {
-      title: this.title,
-      meta: [
-        // Each object in this array is its own meta tag
-        {
-          hid: "description",
-          name: "description",
-          content: "Portfolio with the work I've done for my clients.",
-        },
-      ],
-      bodyAttrs: {
-        class: "portfolio",
-      },
-    };
-  },
-  methods: {
-    getPortfolioImgUrl(pic) {
-      return require("~/assets/images/portfolio/" + pic);
+    components: {
+        Header,
+        Footer,
+        WorkBanner,
     },
-  },
+    data() {
+        return {
+            prefix: "portfolio",
+            title: "Portfolio - Sander Janssen",
+            portfolioItems: [
+                {
+                    title: "Boonstra Marketing",
+                    categories: "Web development",
+                    description:
+                        "Custom WordPress theme for Boonstra Marketing. Design by designpraktijk.nl",
+                    image: "boonstramarketing.jpg",
+                    url: "boonstramarketing.nl",
+                },
+                {
+                    title: "Advocatenkantoor Vos",
+                    categories: "Web development",
+                    description:
+                        "Custom WordPress theme for Advocatenkantoor Vos. Design by worck.nl",
+                    image: "advocatenkantoor-vos.jpg",
+                    url: "advocatenkantoorvos.nl",
+                },
+                {
+                    title: "Smaak Groenten",
+                    categories: "Web design & development",
+                    description:
+                        "Custom WordPress theme with WooCommerce integration for Smaak Groenten.",
+                    image: "smaakgroenten.jpg",
+                    url: "smaakgroenten.nl",
+                },
+                {
+                    title: "Chloé - Events & Productie",
+                    categories: "Web design & development",
+                    description:
+                        "Custom carrd.co website for an independent event & production agency.",
+                    image: "chloe-events.jpg",
+                    url: "chloe-events.nl",
+                },
+                {
+                    title: "Zweedse Pellets",
+                    categories: "Web design & development",
+                    description:
+                        "WordPress E-commerce website using a custom WordPress theme and custom Gutenberg blocks.",
+                    image: "zweedsepellets.jpg",
+                    url: "zweedsepellets.nl",
+                },
+                {
+                    title: "Drumlin Distillery",
+                    categories: "Web design & development",
+                    description:
+                        "WordPress E-commerce website using a custom WordPress theme and custom Gutenberg blocks.",
+                    image: "drumlin-distillery.jpg",
+                    url: "drumlindistillery.com",
+                },
+                {
+                    title: "Else Schaaij",
+                    categories: "Web design & development",
+                    description: "WordPress website for a flute teacher.",
+                    image: "elseschaaij.jpg",
+                    url: "elseschaaij.nl",
+                },
+                {
+                    title: "Relajate Lobitos",
+                    categories: "Web design & development",
+                    description:
+                        "WordPress website for a hostel, restaurant & surfshop in Peru.",
+                    image: "relajate-home.jpg",
+                    url: "relajatelobitos.com",
+                },
+            ],
+        };
+    },
+    head() {
+        return {
+            title: this.title,
+            meta: [
+                // Each object in this array is its own meta tag
+                {
+                    hid: "description",
+                    name: "description",
+                    content:
+                        "Portfolio with the work I've done for my clients.",
+                },
+            ],
+            bodyAttrs: {
+                class: "portfolio",
+            },
+        };
+    },
+    methods: {
+        getPortfolioImgUrl(pic) {
+            return require("~/assets/images/portfolio/" + pic);
+        },
+    },
 };
 </script>
