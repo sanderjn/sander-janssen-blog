d3b99f9d8dd4a82987d4377f3acb0aa488d56133
sanderjn <sander@sanderjanssen.nl>
2023-06-19 20:15:42 +0200

    Add Simple Analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,26 @@
 <template>
-  <div
-    class="
-      w-full
-      min-h-screen
-      bg-gradient-to-b
-      from-gray-50
-      via-gray-100
-      to-gray-100
-    "
-  >
-    <Chat />
-    <Header />
-    <div class="container relative flex-1 py-12 rounded md:py-12">
-      <nuxt />
+    <div
+        class="w-full min-h-screen bg-gradient-to-b from-gray-50 via-gray-100 to-gray-100"
+    >
+        <Chat />
+        <Header />
+        <div class="container relative flex-1 py-12 rounded md:py-12">
+            <nuxt />
+        </div>
+        <Footer />
+        <!-- Simple Analytics - 100% privacy-first analytics -->
+        <script
+            async
+            defer
+            src="https://scripts.simpleanalyticscdn.com/latest.js"
+        ></script>
+        <noscript
+            ><img
+                src="https://queue.simpleanalyticscdn.com/noscript.gif"
+                alt=""
+                referrerpolicy="no-referrer-when-downgrade"
+        /></noscript>
     </div>
-    <Footer />
-  </div>
 </template>
 
 <script>
@@ -24,10 +29,10 @@ import Header from "~/components/Header.vue";
 import Footer from "~/components/Footer.vue";
 
 export default {
-  components: {
-    Chat,
-    Header,
-    Footer,
-  },
+    components: {
+        Chat,
+        Header,
+        Footer,
+    },
 };
 </script>
